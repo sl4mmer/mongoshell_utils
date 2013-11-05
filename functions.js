6b3099c74b1f56614309b367f9f73aa5b053d446
Dmitry <sl4mmer@list.ru>
2013-11-05 10:48:39 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,13 @@
+DBCollection.prototype.findTS = function(query , fields , limit , skip ){
+     var result=this.find(query , fields , limit , skip ).toArray();
+     for (var i=0;i<result.length;i++){
+     	result[i]._id.getTimestamp();
+     	result[i]['__TS__']=result[i]._id.getTimestamp();
+     }
+     return result
+}
+
+
 //Removes data from all collections (except system.indexes) of current database
 function removeData
   var list=db.getCollectionNames();
